62ba814d17c0ee2378bc9c5c907bd8e8f155c5a8
Kiree Bellamy <kireebellamy@gmail.com>
2024-03-07 13:08:44 +0000

    Added basic structure and initial question array

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+// Define an array to hold your quiz questions
+const questions = [
+    {
+        question: "Who do the Borogravians worship?",
+        options: ["Nuggan, Om, Nugget, Anoia"],
+        correctAnswer: "Nuggan" 
+    }
+    {
+        question: "Whats the name of the Terry Pratchett's book set on a fictional Pacific Island devistated by a tsumami?",
+        options: ["Nation, Island, Country,	State"],
+        correctAnswer: "Nation" 
+    }
+    {
+        question: "Sam Vimes fights the summoning dark with?",
+        options: ["Gaurding Dark, Sentry Dark, Protecting Dark, Shielding Dark"],
+        correctAnswer: "Gaurding Dark" 
+    }
+    {
+        question: "What is the Ankh Morpork economic standard after 'Making Money'?",
+        options: ["Golems, Gold, Stamps, Cabbages"],
+        correctAnswer: "Golems" 
+    }
+    {
+        question: "Terry Pratchett colaborated with Neil Gaman on which book?",
+        options: ["Good Omens, Good Intentions,	Interesting Times, The Sandman"],
+        correctAnswer: "Good Omens" 
+    }
+]
+// I plan to add more questions to the array
