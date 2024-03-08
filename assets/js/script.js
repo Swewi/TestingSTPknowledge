9cfb7163bd02f9ce675965fc55930c7856d36e5a
Kiree Bellamy <kireebellamy@gmail.com>
2024-03-08 12:29:24 +0000

    All js code added, but not working correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,68 +1,127 @@
 // Define an array to hold your quiz questions
-const questions = [
-    {
+const questions = [{
         question: "Who do the Borogravians worship?",
-        answers: [
-            {text: "Nuggan", correct: true},
-            {text: "Om", correct: false},
-            {text: "Nugget", correct: false},
-            {text: "Anoia", correct: false},
+        answers: [{
+                text: "Nuggan",
+                correct: true
+            },
+            {
+                text: "Om",
+                correct: false
+            },
+            {
+                text: "Nugget",
+                correct: false
+            },
+            {
+                text: "Anoia",
+                correct: false
+            },
         ]
     },
     {
         question: "Whats the name of the Terry Pratchett's book set on a fictional Pacific Island devistated by a tsumami?",
-        answers: [
-            {text:"Nation", correct: true},
-            {text:"Island", correct: false},
-            {text:"Country", correct: false},
-            {text:"State", correct: false},
+        answers: [{
+                text: "Nation",
+                correct: true
+            },
+            {
+                text: "Island",
+                correct: false
+            },
+            {
+                text: "Country",
+                correct: false
+            },
+            {
+                text: "State",
+                correct: false
+            },
         ]
     },
     {
         question: "Sam Vimes fights the summoning dark with?",
-        answers: [
-            {text:"Gaurding Dark", correct: true},
-            {text:"Sentry Dark", correct: false},
-            {text:"Protecting Dark", correct: false},
-            {text:"Shielding Dark", correct: false},
+        answers: [{
+                text: "Gaurding Dark",
+                correct: true
+            },
+            {
+                text: "Sentry Dark",
+                correct: false
+            },
+            {
+                text: "Protecting Dark",
+                correct: false
+            },
+            {
+                text: "Shielding Dark",
+                correct: false
+            },
         ]
     },
     {
         question: "What is the Ankh Morpork economic standard after 'Making Money'?",
-        answers: [
-            {text:"Golems", correct: true},
-            {text:"Gold", correct: false},
-            {text:"Stamps", correct: false},
-            {text:"Cabbages", correct: false},
+        answers: [{
+                text: "Golems",
+                correct: true
+            },
+            {
+                text: "Gold",
+                correct: false
+            },
+            {
+                text: "Stamps",
+                correct: false
+            },
+            {
+                text: "Cabbages",
+                correct: false
+            },
         ]
     },
     {
         question: "Terry Pratchett colaborated with Neil Gaman on which book?",
-        answers: [
-            {text:"Good Omens", correct: true},
-            {text:"Good Intentions", correct: false},
-            {text:"Interesting Times", correct: false},
-            {text:"The Sandman", correct: false},
+        answers: [{
+                text: "Good Omens",
+                correct: true
+            },
+            {
+                text: "Good Intentions",
+                correct: false
+            },
+            {
+                text: "Interesting Times",
+                correct: false
+            },
+            {
+                text: "The Sandman",
+                correct: false
+            },
         ]
     }
 ];
 // I plan to add more questions to the array
 
+// Defining game elements
+
 const questionElement = document.getElementById(question);
-const answerButton = document.getElementById(answer-btn);
-const nextButton = document.getElementById(next-btn);
+const answerButtons = document.getElementById(answer - buttons);
+const nextButton = document.getElementById(next - btn);
 
 let currentQuestionIndex = 0;
 let score = 0;
 
-function startQuizz(){
+function startQuiz() {
     currentQuestionIndex = 0;
     score = 0;
     nextButton.innerHTML = "Next";
     showQuestion();
 }
 
-function showQuestion(){
+// Making the buttons function, and change
+
+function showQuestion() {
+    resetState();
     let currentQuestion = questions[currentQuestionIndex];
     let questionno = currentQuestionIndex + 1;
     questionElement.innerHTML = questionno + ". " + currentQuestion.question;
@@ -71,8 +130,63 @@ function showQuestion(){
         const button = document.createElement("button");
         button.innerHTML = answer.text;
         button.classList.add("btn");
-        answerButton.appendChild(button);
+        answerButtons.appendChild(button);
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
     });
 }
 
-startQuizz();
+// Reseting the answer button options
+
+function resetState() {
+    nextButton.style.display = "none";
+    while (answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild)
+    };
+}
+
+function selectAnswer() {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if (isCorrect) {
+        selectedBtn.classList.add("correct");
+        score++;
+    } else {
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if (button.dataset.correct === "true") {
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
+
+function showScore() {
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again?";
+    nextButton.style.display = "block";
+}
+
+function handleNextButton() {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+        showQuestion();
+    } else {
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", () => {
+    if (currentQuestionIndex < questions.length) {
+        handleNextButton();
+    } else {
+        startQuiz();
+    }
+})
+
+startQuiz();
