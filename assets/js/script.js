ee660424bf2310977adfd11da83ca60e883968a8
Kiree Bellamy <kireebellamy@gmail.com>
2024-03-08 09:46:28 +0000

    reworked the question array format

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,49 @@
-
-
 // Define an array to hold your quiz questions
 const questions = [
     {
         question: "Who do the Borogravians worship?",
-        options: ["Nuggan, Om, Nugget, Anoia"],
-        correctAnswer: "Nuggan"
+        options: [
+            {text: "Nuggan", correct: true},
+            {text: "Om", correct: false},
+            {text: "Nugget", correct: false},
+            {text: "Anoia", correct: false},
+        ]
     },
     {
         question: "Whats the name of the Terry Pratchett's book set on a fictional Pacific Island devistated by a tsumami?",
-        options: ["Nation, Island, Country, State"],
-        correctAnswer: "Nation"
+        options: [
+            {text:"Nation", correct: true},
+            {text:"Island", correct: false},
+            {text:"Country", correct: false},
+            {text:"State", correct: false},
+        ]
     },
     {
         question: "Sam Vimes fights the summoning dark with?",
-        options: ["Gaurding Dark, Sentry Dark, Protecting Dark, Shielding Dark"],
-        correctAnswer: "Gaurding Dark"
+        options: [
+            {text:"Gaurding Dark", correct: true},
+            {text:"Sentry Dark", correct: false},
+            {text:"Protecting Dark", correct: false},
+            {text:"Shielding Dark", correct: false},
+        ]
     },
     {
         question: "What is the Ankh Morpork economic standard after 'Making Money'?",
-        options: ["Golems, Gold, Stamps, Cabbages"],
-        correctAnswer: "Golems"
+        options: [
+            {text:"Golems", correct: true},
+            {text:"Gold", correct: false},
+            {text:"Stamps", correct: false},
+            {text:"Cabbages", correct: false},
+        ]
     },
     {
         question: "Terry Pratchett colaborated with Neil Gaman on which book?",
-        options: ["Good Omens, Good Intentions, Interesting Times, The Sandman"],
-        correctAnswer: "Good Omens"
+        options: [
+            {text:"Good Omens", correct: true},
+            {text:"Good Intentions", correct: false},
+            {text:"Interesting Times", correct: false},
+            {text:"The Sandman", correct: false},
+        ]
     }
 ];
 // I plan to add more questions to the array
