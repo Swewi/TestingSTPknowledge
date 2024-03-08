dfdcf1fbf9e7173e98d1ed389f4ae55a1fec5111
Kiree Bellamy <kireebellamy@gmail.com>
2024-03-08 11:00:44 +0000

    defining answer-btn is not working here

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@
 const questions = [
     {
         question: "Who do the Borogravians worship?",
-        options: [
+        answers: [
             {text: "Nuggan", correct: true},
             {text: "Om", correct: false},
             {text: "Nugget", correct: false},
@@ -11,7 +11,7 @@ const questions = [
     },
     {
         question: "Whats the name of the Terry Pratchett's book set on a fictional Pacific Island devistated by a tsumami?",
-        options: [
+        answers: [
             {text:"Nation", correct: true},
             {text:"Island", correct: false},
             {text:"Country", correct: false},
@@ -20,7 +20,7 @@ const questions = [
     },
     {
         question: "Sam Vimes fights the summoning dark with?",
-        options: [
+        answers: [
             {text:"Gaurding Dark", correct: true},
             {text:"Sentry Dark", correct: false},
             {text:"Protecting Dark", correct: false},
@@ -29,7 +29,7 @@ const questions = [
     },
     {
         question: "What is the Ankh Morpork economic standard after 'Making Money'?",
-        options: [
+        answers: [
             {text:"Golems", correct: true},
             {text:"Gold", correct: false},
             {text:"Stamps", correct: false},
@@ -38,7 +38,7 @@ const questions = [
     },
     {
         question: "Terry Pratchett colaborated with Neil Gaman on which book?",
-        options: [
+        answers: [
             {text:"Good Omens", correct: true},
             {text:"Good Intentions", correct: false},
             {text:"Interesting Times", correct: false},
@@ -48,5 +48,31 @@ const questions = [
 ];
 // I plan to add more questions to the array
 
+const questionElement = document.getElementById(question);
+const answerButton = document.getElementById(answer-btn);
+const nextButton = document.getElementById(next-btn);
 
+let currentQuestionIndex = 0;
+let score = 0;
 
+function startQuizz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion(){
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionno = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionno + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButton.appendChild(button);
+    });
+}
+
+startQuizz();
